3c298d7f69f3d465df8fa38714f221d05901470d
bharatpathak1998 <bharatpathak099@gmail.com>
2022-12-23 14:17:46 +0530

    Added : Functionality To Check Password Valid Or Not

--- UserRegistration.java
+++ UserRegistration.java
@@ -6,7 +6,7 @@ public class UserRegistration {
 
     public void userPassword(String password) {
 
-        Pattern pattern = Pattern.compile("[a-zA-Z0-9]{8,}");
+        Pattern pattern = Pattern.compile("[a-zA-Z0-9][.*@]?.{8,}");
         Matcher matcher = pattern.matcher(password);
 
         if (matcher.find()) {
